b33bab02fff870c63ebb524c0dee57d2f6959de4
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-02-29 10:15:27 -0500

    Add files via upload

---
+++ project02-02.js
@@ -0,0 +1,21 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Mark Gore
+      Date:  2/29/2024
+
+      Filename: project02-02.js
+ */
+ function verifyForm(){
+	 let name = document.getElementById("name").value;
+	 let email = document.getElementById("email").value;
+	 let phone = document.getElementById("phone").value;
+	 
+	 (name && email && phone) ?
+	 window.alert("Thank you!") :
+	 window.alert("Please fill in all fields.")
+ }
+ 
+document.getElementById("submit").addEventListener("click", verifyForm);
